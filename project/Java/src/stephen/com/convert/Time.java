8ba381127a9d0835ad6fe646c12ac3e29ab061a1
stephenCuibo <610201092@qq.com>
2017-10-27 00:49:21 +0800

    add

---
+++ project/Java/src/stephen/com/convert/Time.java
@@ -0,0 +1,11 @@
+package stephen.com.convert;
+
+public class Time {
+
+	public static void main(String[] args) {
+
+	}
+	
+	
+
+}
---
+++ project/kafkatest/KafkaTest/src/main/java/KafkaTest/KafkaTest/KafkaConsumerProducerDemo.java
@@ -0,0 +1,16 @@
+package KafkaTest.KafkaTest;
+
+
+/**
+* @author leicui bourne_cui@163.com
+*/
+public class KafkaConsumerProducerDemo
+{
+    public static void main(String[] args)
+    {
+        KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);
+        producerThread.start();
+        KafkaConsumer consumerThread = new KafkaConsumer(KafkaProperties.topic);
+        consumerThread.start();
+    }
+}
