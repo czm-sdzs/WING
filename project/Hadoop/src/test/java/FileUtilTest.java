57dcd34f49250dc35fd76517ef6cb804d1d4f6c0
stephenCuibo <610201092@qq.com>
2017-12-04 22:17:34 +0800

    add

---
+++ project/Hadoop/src/test/java/FileUtilTest.java
@@ -0,0 +1,68 @@
+/**
+ * Created by Administrator on 2017/11/22.
+ */
+
+import com.wing.ace.bigdata.component.api.hdfs.FileUtil;
+import org.junit.Test;
+
+/**
+ * description 文件操作工具类测试
+ * author stephen.cui
+ * date 2017/11/22 15:23
+ **/
+public class FileUtilTest {
+    private static FileUtil fileUtil = new FileUtil();
+
+    @Test
+    public void readLocalFileTest() {
+        String localtext = "pom.xml";
+        fileUtil.readLocalFile(localtext);
+    }
+
+    @Test
+    public void readHdfsTest() {
+        String hdfsFile = "/data/file/word.txt";
+        fileUtil.readHdfsFile(hdfsFile);
+    }
+
+    @Test
+    public void copyLocal2HdfsTest(){
+        String localPash = "E:\\git_repository\\pro\\WING\\project\\springBootRest\\data\\bigLottoData\\BigLotto.txt";
+        String targetPath = "/tmp/log.log";
+        fileUtil.copyLocalData2Hdfs(localPash,targetPath);
+    }
+
+    @Test
+    public void isExistTest(){
+        String pathStr = "/data";
+        fileUtil.isExist(pathStr);
+    }
+
+    @Test
+    public void createFolder(){
+        String folderStr="/tmp/0";
+        fileUtil.createFolder(folderStr);
+    }
+    @Test
+    public void createFileTest(){
+        String pathStr="/tmp/2";
+        fileUtil.createFile(pathStr);
+    }
+
+    @Test
+    public void isExistAndIsCreateTest(){
+        String pathStr = "/tmp/5/log.log";
+        fileUtil.isExistAndIsCreate(pathStr,true);
+    }
+
+    /**
+     * 删除测试
+     */
+    @Test
+    public void rmrTest(){
+        String folderStr = "/tmp/5";
+        String fileStr = "/tmp/2";
+        fileUtil.rmr(folderStr);
+    }
+
+}
