a8bf416a78174c6875725110bd04660c02aff8c0
stephenCuibo <610201092@qq.com>
2018-03-08 16:16:10 +0800

    add RSA

---
+++ project/Java/src/main/java/com/wing/ace/common/data_sign/Data.java
@@ -0,0 +1,40 @@
+package com.wing.ace.common.data_sign;
+
+import java.util.List;
+
+/**
+ * Create By: CuiBo
+ * Date: 2018/3/8 11:48
+ * Description:
+ */
+public class Data {
+
+
+    public String data;
+    public String signData;
+    public String flag;
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getSignData() {
+        return signData;
+    }
+
+    public void setSignData(String signData) {
+        this.signData = signData;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+}
