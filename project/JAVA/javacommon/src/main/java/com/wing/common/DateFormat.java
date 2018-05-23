ee763effe9536910a461e6cb3e3718795b5d12b3
stephenCuibo <610201092@qq.com>
2018-05-23 15:11:19 +0800

    update

--- project/JAVA/javacommon/src/main/java/com/wing/common/DateFormat.java
+++ project/JAVA/javacommon/src/main/java/com/wing/common/DateFormat.java
@@ -1,37 +1,37 @@
-package com.wing.common;//package com.wing.common;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//
-///**
-// * Create By: CuiBo
-// * Date: 2018/3/5 15:57
-// * Description:
-// */
-//public class DateFormat {
-//
-//    private static Logger LOG = LoggerFactory.getLogger(DateFormat.class);
-//
-//    /**
-//     * @Author: CuiBo
-//     * @Params:
-//     * @Return:
-//     * @Date : 2018/3/5 15:57
-//     */
-//    public static void getNowDate() {
-//        Date currentTime = new Date();
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        String dateString = formatter.format(currentTime);
-//        System.out.println(dateString);
-//        LOG.error("dateString:{}",dateString);
-//
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        getNowDate();
-//    }
-//}
+package com.wing.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Create By: CuiBo
+ * Date: 2018/3/5 15:57
+ * Description:
+ */
+public class DateFormat {
+
+    private static Logger LOG = LoggerFactory.getLogger(DateFormat.class);
+
+    /**
+     * @Author: CuiBo
+     * @Params:
+     * @Return:
+     * @Date : 2018/3/5 15:57
+     */
+    public static void getNowDate() {
+        Date currentTime = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = formatter.format(currentTime);
+        System.out.println(dateString);
+        LOG.error("dateString:{}",dateString);
+
+    }
+
+
+    public static void main(String[] args) {
+        getNowDate();
+    }
+}
--- project/JAVA/javacommon/src/main/java/com/wing/common/jdbc/JdbcUtil.java
+++ project/JAVA/javacommon/src/main/java/com/wing/common/jdbc/JdbcUtil.java
@@ -1,62 +1,62 @@
-package com.wing.common.jdbc;//package com.wing.common.jdbc;
-//
-//import com.wing.common.PropertiesLoaderUtil;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.sql.SQLException;
-//import java.util.Hashtable;
-//
-///**
-// * Created by Administrator on 2017/11/21.
-// */
-//public class JdbcUtil {
-//
-//    /**
-//     *
-//     */
-//    public static  Connection  getConnect(){
-//        PropertiesLoaderUtil rp = new PropertiesLoaderUtil();
-//        Connection conn =null;
-//        Hashtable hashtable = rp.getC3p0Property();
-//        String driver = hashtable.get("driver-class-name").toString();
-//        String url = hashtable.get("url").toString();
-//        String username = hashtable.get("username").toString();
-//        String password = hashtable.get("password").toString();
-//        try {
-//            Class.forName(driver);
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            conn = (Connection) DriverManager.getConnection(url, username, password);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return conn;
-//    }
-//
-//    public static void psClose(PreparedStatement ps){
-//        if(ps !=null){
-//            try {
-//                ps.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public static void connClose(Connection conn){
-//        if(conn != null){
-//            try {
-//                conn.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//    }
-//
-//
-//}
+package com.wing.common.jdbc;
+
+import com.wing.common.PropertiesLoaderUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Hashtable;
+
+/**
+ * Created by Administrator on 2017/11/21.
+ */
+public class JdbcUtil {
+
+    /**
+     *
+     */
+    public static  Connection  getConnect(){
+        PropertiesLoaderUtil rp = new PropertiesLoaderUtil();
+        Connection conn =null;
+        Hashtable hashtable = rp.getC3p0Property();
+        String driver = hashtable.get("driver-class-name").toString();
+        String url = hashtable.get("url").toString();
+        String username = hashtable.get("username").toString();
+        String password = hashtable.get("password").toString();
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void psClose(PreparedStatement ps){
+        if(ps !=null){
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void connClose(Connection conn){
+        if(conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
